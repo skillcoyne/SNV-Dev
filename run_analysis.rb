e58d4deaeef04c4363f0c193107e7ac0eff73e4f
Sarah Killcoyne <skillcoyne@gmail.com>
2012-05-07 10:43:10 +0200

    permissions updates

--- run_analysis.rb
+++ run_analysis.rb
@@ -17,7 +17,7 @@ def write_scripts(opts = {})
     filename = File.basename(opts[:input_file], ".mdr")
 
     File.open("#{opts[:output_path]}/#{filename}_#{$script}", 'w') { |f| f.write(r_script) }
-    FileUtils.chmod(0755, "#{opts[:output_path]}/#{filename}_#{$script}")
+    FileUtils.chmod(0776, "#{opts[:output_path]}/#{filename}_#{$script}")
 
     opts[:r_script] = "#{opts[:output_path]}/#{filename}_#{$script}"
     write_oar_file(opts)
@@ -43,7 +43,7 @@ kash -M ${OAR_NODEFILE} -- ${PROGNAME} \$TAKTUK_COUNT \$TAKTUK_RANK
 OAR
 
   File.open("#{opts[:oar]}/oar_launcher.#{filename}.sh", 'w') { |f| f.write(oar_script) }
-  FileUtils.chmod(0755, "#{opts[:oar]}/oar_launcher.#{filename}.sh")
+  FileUtils.chmod(0776, "#{opts[:oar]}/oar_launcher.#{filename}.sh")
 end
 
 def run_scripts(script_path)
