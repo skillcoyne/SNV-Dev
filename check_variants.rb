26b155be4a9df742ce6888a26197f2c99ca65e79
Sarah Killcoyne <skillcoyne@gmail.com>
2012-10-03 12:27:16 +0200

    Parsing snp DocSet descriptions

--- check_variants.rb
+++ check_variants.rb
@@ -2,6 +2,15 @@ require 'fileutils'
 require 'yaml'
 require_relative 'lib/variant'
 
+def write_variant_file(file, vars)
+  file.write("dbsnpID\tMAF\tFunction\tChromosome\n")
+  vars.each_pair do |id, variant|
+    file.write("#{variant.id}\t#{variant.frequency}\t#{variant.type}\t#{variant.chromosome}\n")
+    print "#{variant.id}\t#{variant.frequency}\t#{variant.type}\t#{variant.chromosome}\n"
+  end
+end
+
+
 unless ARGV.length > 0
   warn("DocSet snp descriptions file required.")
   exit
@@ -11,6 +20,7 @@ end
 file = ARGV[0]
 
 entries = Hash.new
+not_valid = Hash.new
 var = Variant.new
 
 File.open(file, 'r') do |infile|
@@ -23,15 +33,18 @@ File.open(file, 'r') do |infile|
     end
 
     if (line.match(/rs\d+/) or infile.eof) and infile.lineno > 1
-      if entries.has_key?id
-          unless var.eql?(entries[id])
-            puts "#{id} needs to be looked at"
-          end
+      if entries.has_key? id
+        unless var.eql?(entries[id])
+          puts "#{id} needs to be looked at"
+        end
       end
-
-      entries[id] = var if var.validated #and maf
+      if var.validated #and maf
+        entries[id] = var
+      else
+        not_valid[id] = var
+      end
       id = nil
-      line =~ /rs\d+ has merged into (rs\d+)/ ? id = $1: id = line.match(/rs\d+/)
+      line =~ /rs\d+ has merged into (rs\d+)/ ? id = $1 : id = line.match(/rs\d+/)
       var = Variant.new(id)
     end
 
@@ -41,7 +54,7 @@ File.open(file, 'r') do |infile|
       when /^VALIDATED/
         var.validated = true unless line =~ /no-info/
       when /^FXN_CLASS/
-        var.type = line.sub("FXN_CLASS=","")
+        var.type = line.sub("FXN_CLASS=", "")
       when /^CHR=/
         var.chromosome = line.sub("CHR=", "")
     end
@@ -50,10 +63,6 @@ File.open(file, 'r') do |infile|
 end
 
 
-File.open("data/variants.txt", 'w') {|f|
-  f.write("dbsnpID\tMAF\tFunction\tChromosome\n")
-  entries.each_pair do |id, variant|
-    f.write( "#{variant.id}\t#{variant.frequency}\t#{variant.type}\t#{variant.chromosome}\n"  )
-  end
-}
+write_variant_file(File.open("data/dbsnp_validated_cg_variants.txt", 'w'), entries)
+write_variant_file(File.open("data/unvalidated_cg_variants.txt", 'w'), not_valid)
 
